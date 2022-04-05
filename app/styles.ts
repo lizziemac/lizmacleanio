c84f0f6907ea9e3750abaaf74d844347978b9fda
Liz MacLean <elizabeth.maclean95@gmail.com>
2022-04-04 21:31:59 -0400

    Add cooler formatting to code blocks; update verbiage for deploy workflow (#11)

--- app/styles.ts
+++ app/styles.ts
@@ -15,7 +15,7 @@ export const GlobalStyles = createGlobalStyle<DefaultTheme>`
     background-color: ${({ theme }: {theme: Theme}): string => theme.primaryColor};
     color: ${({ theme }: {theme: Theme}): string => theme.textColor};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
-    transition: background-color 1.5s linear;
+    transition: background-color 1.5s ease;
 
     @media only screen and (max-width:1000px){
       font-size: ${({ theme }: {theme: Theme}): string => theme.mediaFontSize};
@@ -24,8 +24,13 @@ export const GlobalStyles = createGlobalStyle<DefaultTheme>`
   }
 
   code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+    padding: 0.2em 0.4em;
+    margin: 0;
+    font-size: 85%;
+    background-color: ${({ theme }: {theme: Theme}): string => theme.primaryDarkColor};
+    transition: background-color 1.5s ease;
+    border-radius: 6px;
   }
 
   .App {
