4f2d909902a54d7462902e05e853efd0182cfcd0
Liz MacLean <elizabeth.maclean95@gmail.com>
2022-04-17 17:37:27 -0400

    Add i18next linting (#32)

--- app/pages/Home/index.tsx
+++ app/pages/Home/index.tsx
@@ -7,6 +7,7 @@ const Home = (): ReactElement => {
       <h1>
         {translate(PAGES.HOME.TITLE)}
         <p>{translate(PAGES.HOME.SUBTITLE)}<br/>
+          { /* eslint-disable-next-line i18next/no-literal-string */}
           <code>my_list = [ key for key in item_dict.keys() ]</code>
         </p>
       </h1>
