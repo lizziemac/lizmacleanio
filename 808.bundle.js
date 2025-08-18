d6c2b83bac8d086ca36f4250d578cbabffdd0d1b
lizziemac <18120837+lizziemac@users.noreply.github.com>
2025-08-18 02:06:10 +0000

    Deploying to dist-build from @ lizziemac/lizmacleanio@499127f9f892cbe29818767d009b2053c32090f9 🚀

---
+++ 808.bundle.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunklizmacleanio=self.webpackChunklizmacleanio||[]).push([[808],{808:(e,i,t)=>{t.r(i),t.d(i,{default:()=>l});var r=t(848),s=t(802);const a=s.Ay.div`
+  width: 80%;
+  aspect-ratio: 16 / 9;
+  padding-top: 1em;
+
+  @media screen and (max-width:1000px) {
+    padding-top: 0.25em;
+    width: 100%;
+    aspect-ratio: 16 / 9;
+  }
+`,d=s.Ay.iframe`
+  width: 100%;
+  height: 100%;
+`;var c=t(505);const l=()=>(0,r.jsxs)("div",{children:[(0,r.jsxs)("h1",{children:[(0,r.jsx)("div",{style:{height:"2em"}}),c.Ay.t(c.Nb.HOME.TITLE)]}),(0,r.jsxs)("div",{style:{display:"flex",flexDirection:"column",alignItems:"center",padding:"1em"},children:[(0,r.jsxs)("p",{children:[c.Ay.t(c.Nb.HOME.SUBTITLE),(0,r.jsx)("br",{})]}),(0,r.jsx)(a,{children:(0,r.jsx)(d,{width:"100%",height:"100%",src:"https://www.youtube.com/embed/LCUze7kuNas?si=dICdwOPkI11dqNit&start=1",title:"Stop motion: Parks and Recreation",frameBorder:"0",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",referrerPolicy:"strict-origin-when-cross-origin",allowFullScreen:!0})})]})]})}}]);
